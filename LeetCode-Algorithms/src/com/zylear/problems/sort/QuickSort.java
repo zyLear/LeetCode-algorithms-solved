0cd52626fb2a680332039b0e691d31ec281d9d4e
zylear <284449710@qq.com>
2020-02-12 17:20:23 +0800

    quickSort

---
+++ LeetCode-Algorithms/src/com/zylear/problems/sort/QuickSort.java
@@ -0,0 +1,65 @@
+package com.zylear.problems.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by xiezongyu on 2020/2/11.
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        Integer[] array = new Integer[]{1231, 234, 4, 86, 21, 6, 1, 4, 68, 34, 3, 3, 4, 7, 2, 2, 3, 4};
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.asList(array));
+    }
+
+
+    public static void quickSort(Integer[] array, int low, int high) {
+        int lowIndex, highIndex, temp;
+        if (low >= high) {
+            return;
+        }
+        lowIndex = low;
+        highIndex = high;
+        //temp就是基准位
+        temp = array[low];
+
+        while (lowIndex < highIndex) {
+
+            while (lowIndex < highIndex) {
+                if (temp > array[highIndex]) {
+                    swap(array, lowIndex, highIndex);
+                    break;
+                } else {
+                    highIndex--;
+                }
+            }
+
+            while (lowIndex < highIndex) {
+                if (temp < array[lowIndex]) {
+                    swap(array, lowIndex, highIndex);
+                    break;
+                } else {
+                    lowIndex++;
+                }
+            }
+
+
+        }
+
+        //递归调用左半数组
+        quickSort(array, low, highIndex - 1);
+        //递归调用右半数组
+        quickSort(array, highIndex + 1, high);
+    }
+
+    private static void swap(Integer[] array, int lowIndex, int highIndex) {
+
+        int temp = array[lowIndex];
+        array[lowIndex] = array[highIndex];
+        array[highIndex] = temp;
+
+    }
+
+
+}
