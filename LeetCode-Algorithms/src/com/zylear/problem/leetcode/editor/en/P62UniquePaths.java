fd21315504d0a8eeb9941d798c5f90b31492470c
zylear <284449710@qq.com>
2022-10-11 18:02:36 +0800

    simple unique path

--- LeetCode-Algorithms/src/com/zylear/problem/leetcode/editor/en/P62UniquePaths.java
+++ LeetCode-Algorithms/src/com/zylear/problem/leetcode/editor/en/P62UniquePaths.java
@@ -63,7 +63,7 @@ public class P62UniquePaths {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int uniquePaths(int m, int n) {
+        public int uniquePaths1(int m, int n) {
             int[][] dp = new int[m + 2][n + 2];
             dp[m][n] = 1;
             for (int i = m; i > 0; i--) {
@@ -75,6 +75,19 @@ public class P62UniquePaths {
 
             }
             return dp[1][1];
+        }
+
+        public int uniquePaths(int m, int n) {
+            int[][] dp = new int[m + 1][n + 1];
+            dp[1][1] = 1;
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (!(i == 1 && j == 1)) {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    }
+                }
+            }
+            return dp[m][n];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ LeetCode-Algorithms/src/com/zylear/problem/leetcode/editor/en/doc/content/P5LongestPalindromicSubstring.md
@@ -0,0 +1,29 @@
+<p>Given a string <code>s</code>, return <em>the longest palindromic substring</em> in <code>s</code>.</p>
+
+<p>A string is called a palindrome string if the reverse of that string is the same as the original string.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "babad"
+<strong>Output:</strong> "bab"
+<strong>Explanation:</strong> "aba" is also a valid answer.
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "cbbd"
+<strong>Output:</strong> "bb"
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>1 &lt;= s.length &lt;= 1000</code></li> 
+ <li><code>s</code> consist of only digits and English letters.</li> 
+</ul>
+
+<div><div>Related Topics</div><div><li>String</li><li>Dynamic Programming</li></div></div><br><div><li>👍 21958</li><li>👎 1263</li></div>
