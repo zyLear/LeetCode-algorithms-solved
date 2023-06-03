983af01f41dae2f7375328863cbf49b93a1c9e09
zylear <284449710@qq.com>
2023-06-03 14:49:21 +0800

    some sql

---
+++ LeetCode-Algorithms/src/com/zylear/problem/leetcode/editor/en/P193ValidPhoneNumbers.java
@@ -0,0 +1,41 @@
+package com.zylear.problem.leetcode.editor.en;
+
+//Given a text file file.txt that contains a list of phone numbers (one per 
+//line), write a one-liner bash script to print all valid phone numbers. 
+//
+// You may assume that a valid phone number must appear in one of the following 
+//two formats: (xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit) 
+//
+// You may also assume each line in the text file must not contain leading or 
+//trailing white spaces. 
+//
+// Example: 
+//
+// Assume that file.txt has the following content: 
+//
+// 
+//987-123-4567
+//123 456 7890
+//(123) 456-7890
+// 
+//
+// Your script should output the following valid phone numbers: 
+//
+// 
+//987-123-4567
+//(123) 456-7890
+// 
+//
+// Related Topics Shell 👍 357 👎 880
+
+
+public class P193ValidPhoneNumbers {
+    public static void main(String[] args) {
+//        Solution solution = new P193ValidPhoneNumbers().new Solution();
+    }
+    
+//There is no code of Java type for this problem
+
+    //-E '^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$'
+    
+}
