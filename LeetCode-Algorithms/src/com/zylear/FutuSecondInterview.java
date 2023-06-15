12bed3bedf7764c75ea02329df656a51854e03ea
zylear <284449710@qq.com>
2023-06-15 15:07:00 +0800

    futu second interview

---
+++ LeetCode-Algorithms/src/com/zylear/FutuSecondInterview.java
@@ -0,0 +1,26 @@
+package com.zylear;
+
+public class FutuSecondInterview {
+
+    public static void main(String[] args) {
+
+        System.out.println(handle(3, 15));
+
+    }
+
+    public static double handle(int hours, int minutes) {
+
+        double hoursIndex = hours * 5;
+        double diff = 0;
+
+        hoursIndex = hoursIndex + ((double)minutes / 60) * 5;
+
+        diff = Math.abs(hoursIndex - minutes);
+
+        if (diff > 30) {
+            diff = 60 - diff;
+        }
+        return ( diff / 60) * 360;
+    }
+
+}
